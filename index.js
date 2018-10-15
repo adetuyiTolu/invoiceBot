2ac9364e83ceb82825e1d32f3dc294ee0aa849d8
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-10-15 19:39:50 +0100

    change server port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(3000, ()=>{
-  console.log("server started at port 3000");
+app.listen(port, ()=>{
+  console.log("server started at port 8080");
 });
