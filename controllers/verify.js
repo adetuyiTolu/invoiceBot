7289feba6b240a4ea88adcb207c4cf7e818b0767
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-10-15 22:32:43 +0100

    add message processor engine

--- controllers/verify.js
+++ controllers/verify.js
@@ -3,8 +3,6 @@ module.exports = (req, res) =>{
   const hubChallenge = req.query['hub.challenge'];
   const hubMode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
-  const fb_token ="EAAGqArAZCv8MBADmiGQ3ruPQxxR5Rj7JmCT567wveZAX9wGmjjwuivaOhT4C0OpYZAbDwBXpZAZC1pD6BthkUj5JMATR0Korc0kMPT9ZCSpb6K7hZBYOfX8rOzeArs1XAWLNEnZCxybJfHbctkEMqXXZBdtq7NVU1lUUnhZCuz5HP10wZDZD"
-
   const isTokenMatch = (token === 'invoice_bot');
 
   if(hubMode&& isTokenMatch){
---
+++ helpers/processMessage.js
@@ -0,0 +1,27 @@
+const API_AI_TOKEN = 'your Dialogflow Client Access Token';
+const FACEBOOK_ACCESS_TOKEN = 'EAAGqArAZCv8MBADmiGQ3ruPQxxR5Rj7JmCT567wveZAX9wGmjjwuivaOhT4C0OpYZAbDwBXpZAZC1pD6BthkUj5JMATR0Korc0kMPT9ZCSpb6K7hZBYOfX8rOzeArs1XAWLNEnZCxybJfHbctkEMqXXZBdtq7NVU1lUUnhZCuz5HP10wZDZD';
+const apiAiClient = require('apiai')(API_AI_TOKEN);
+const sendTextMessage = (senderId, text) =>
+{
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+    method: 'POST',
+    json: {
+      recipient: { id: senderId },
+      message: { text },
+    }
+  });
+};
+
+module.exports = (event) => {
+ const senderId = event.sender.id;
+ const message = event.message.text;
+ const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'invoice_bot'});
+ apiaiSession.on('response', (response) => {
+   const result = response.result.fulfillment.speech;
+   sendTextMessage(senderId, result);
+ });
+ apiaiSession.on('error', error => console.log(error));
+ apiaiSession.end();
+};
